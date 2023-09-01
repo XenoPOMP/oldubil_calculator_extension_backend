2fc9a5ae88d104ef944e4519db3a167198680dbb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-02 00:19:08 +0300

    change env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  require('dotenv').config();
-  const env = process.env;
+	require('dotenv').config();
+	const env = process.env;
 
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(env.APP_PORT);
+	const app = await NestFactory.create(AppModule, { cors: true });
+	await app.listen(env.PORT);
 }
 bootstrap();
